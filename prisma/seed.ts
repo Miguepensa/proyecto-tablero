83813db85ece40a6c04777a7ae14f3a5ee54fc9e
miguel pensado <miguel.pensado@equiposmc.com>
2026-05-13 17:56:16 -0600

    Corregir seed de Prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, UserRole } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
@@ -23,7 +23,7 @@ async function main() {
       name: "Administrador",
       email,
       passwordHash,
-      role: UserRole.ADMIN,
+      role: "ADMIN",
       active: true,
     },
   });
